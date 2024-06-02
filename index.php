944fa87b551a601c0e1ba48b9c32b5b6e5c45ded
Steliyan Nedev <steliyannedev@Steliyans-MacBook-Pro.local>
2024-06-02 22:05:27 +0300

    deployment issues

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', (Config::DEBUG_MODE) ? 'development' : 'production');
+	define('ENVIRONMENT', (Config::$DEBUG_MODE) ? 'development' : 'production');
 
 /*
  *---------------------------------------------------------------
