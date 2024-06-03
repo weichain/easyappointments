84bf96c2e080ece23f499ee2a1c4e5c991deb679
Steliyan Nedev <steliyannedev@Steliyans-MacBook-Pro.local>
2024-06-03 13:01:19 +0300

    deployment issues

--- config.php
+++ config.php
@@ -1,28 +1,72 @@
 <?php
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * author      A.Tselegidis <alextselegidis@gmail.com>
+ * copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
+ * license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * link        http://easyappointments.org
+ * since       v1.0.0
+ * ---------------------------------------------------------------------------- */
 
+/**
+ * Easy!Appointments Configuration File
+ *
+ * Set your installation BASE_URL * without the trailing slash * and the database
+ * credentials in order to connect to the database. You can enable the DEBUG_MODE
+ * while developing the application.
+ *
+ * Set the default language by changing the LANGUAGE constant. For a full list of
+ * available languages look at the /application/config/config.php file.
+ *
+ * IMPORTANT:
+ * If you are updating from version 1.0 you will have to create a new "config.php"
+ * file because the old "configuration.php" is not used anymore.
+ */
 class Config {
-    const BASE_URL = '';
-    const DATABASE_HOST = '';
-    const DATABASE_USER = '';
-    const DATABASE_PASS = '';
-    const DATABASE_NAME = '';
-    const DEBUG_MODE = '';
 
-    public static function loadEnvVars() {
-        self::setConstant('BASE_URL', getenv('BASE_URL'));
-        self::setConstant('DATABASE_HOST', getenv('DATABASE_HOST'));
-        self::setConstant('DATABASE_USER', getenv('DATABASE_USER'));
-        self::setConstant('DATABASE_PASS', getenv('DATABASE_PASS'));
-        self::setConstant('DATABASE_NAME', getenv('DATABASE_NAME'));
-        self::setConstant('DEBUG_MODE', getenv('DEBUG_MODE'));
-    }
+    // ------------------------------------------------------------------------
+    // GENERAL SETTINGS
+    // ------------------------------------------------------------------------
 
-    private static function setConstant($name, $value) {
-        $constantName = 'self::' . $name;
-        if (!defined($constantName)) {
-            define($constantName, $value);
-        }
+    public static $BASE_URL      = 'https://sea-lion-app-fgzhw.ondigitalocean.app/';
+    public static $LANGUAGE      = 'english';
+    public static $DEBUG_MODE    = FALSE;
+
+    // ------------------------------------------------------------------------
+    // DATABASE SETTINGS
+    // ------------------------------------------------------------------------
+
+    public static $DB_HOST       = null;
+    public static $DB_NAME       = null;
+    public static $DB_USERNAME   = null;
+    public static $DB_PASSWORD   = null;
+
+    // ------------------------------------------------------------------------
+    // GOOGLE CALENDAR SYNC
+    // ------------------------------------------------------------------------
+
+    public static $GOOGLE_SYNC_FEATURE   = FALSE;
+    public static $GOOGLE_PRODUCT_NAME   = null;
+    public static $GOOGLE_CLIENT_ID      = null;
+    public static $GOOGLE_CLIENT_SECRET  = null;
+    public static $GOOGLE_API_KEY        = null;
+
+    public static function init() {
+        self::$DB_HOST       = getenv('DB_HOST');
+        self::$DB_NAME       = getenv('DB_NAME');
+        self::$DB_USERNAME   = getenv('DB_USERNAME');
+        self::$DB_PASSWORD   = getenv('DB_PASSWORD');
+        self::$GOOGLE_SYNC_FEATURE   = getenv('GOOGLE_SYNC_FEATURE') === 'true' ? TRUE : FALSE;
+        self::$GOOGLE_PRODUCT_NAME   = getenv('GOOGLE_PRODUCT_NAME');
+        self::$GOOGLE_CLIENT_ID      = getenv('GOOGLE_CLIENT_ID');
+        self::$GOOGLE_CLIENT_SECRET  = getenv('GOOGLE_CLIENT_SECRET');
+        self::$GOOGLE_API_KEY        = getenv('GOOGLE_API_KEY');
     }
 }
 
-Config::loadEnvVars();
+Config::init();
+
+/* End of file config.php */
+/* Location: ./config.php */
